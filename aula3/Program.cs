e15e5d06213a0d9e20bfa2c87cc01d28406019e2
ALUNO <ALUNO@LABFPC36>
2024-02-26 08:49:26 -0300

    Adicionar arquivos de projeto.

---
+++ aula3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+           a = int.Parse(Console.ReadLine());
+           b = int.Parse(Console.ReadLine());
+           c = int.Parse(Console.ReadLine());
+            if (a + b < c || a + c < b || b + c < a)
+            {
+                Console.WriteLine("não compõem o triângulo");
+            }
+            else if (a == b && a == c) 
+            {
+                Console.WriteLine("triangulo equilatero");
+            }
+            else if (a != b && a != c)
+            {
+                Console.WriteLine("triangulo escaleno");
+            }
+            else if (a == b && a != c)
+            {
+                Console.WriteLine("triangulo isoceles");
+            }
+           Console.ReadKey();   
+        }
+    }
+}
